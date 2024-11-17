ccc7738d00f5fda0ee523f3abcfa7e0520917300
adamrodi <adamcrodi123@yahoo.com>
2024-11-17 12:12:16 -0600

    add button event listeners

--- script.js
+++ script.js
@@ -25,8 +25,25 @@ let firstOperand = 100;
 let secondOperand = 80;
 let operator = '+';
 
-console.log(sum(100, 80));
+console.log(add(100, 80));
 console.log(subtract(100, 80));
 console.log(multiply(100, 80));
 console.log(divide(100, 80));
 
+
+const display = document.getElementById('display');
+const operandButtons = document.querySelectorAll('.operand');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+const signButton = document.getElementById('sign');
+const percentButton = document.getElementById('percent');
+const decimalButton = document.getElementById('decimal');
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.textContent);
+    });
+});
