431d06bc2319608c4fe0a208e1ea05f57f0bc338
adamrodi <adamcrodi123@yahoo.com>
2024-11-17 13:30:52 -0600

    add working firstOperand and display

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(firstOperand, operator, secondOperand) {
-    let result = 0;
+function operate() {
     switch (operator) {
-        case '+': result = add(firstOperand, secondOperand);
-        case '-': result = subtract(firstOperand, secondOperand);
-        case '*': result = multiply(firstOperand, secondOperand);
-        case '/': result = divide(firstOperand, secondOperand);
+        case 'add': result = firstOperand + secondOperand;
+        case 'subtract': result = firstOperand - secondOperand;
+        case 'multiply': result = firstOperand * secondOperand;
+        case 'divide': result = firstOperand / secondOperand;
     }
-    return result;
 }
-let firstOperand = 100;
-let secondOperand = 80;
-let operator = '+';
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let result = '0';
 
 console.log(add(100, 80));
 console.log(subtract(100, 80));
@@ -32,18 +19,63 @@ console.log(divide(100, 80));
 
 
 const display = document.getElementById('display');
-const operandButtons = document.querySelectorAll('.operand');
-const operatorButtons = document.querySelectorAll('.operator');
 const equalsButton = document.getElementById('equals');
 const clearButton = document.getElementById('clear');
 const signButton = document.getElementById('sign');
 const percentButton = document.getElementById('percent');
 const decimalButton = document.getElementById('decimal');
-
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        alert(button.textContent);
-    });
-});
+    if (button.classList.contains("operand")) {
+        button.addEventListener("click", () => {
+            inputOperand(button.value)
+            updateDisplay();
+        });
+    }
+    else if (button.classList.contains("operator")) {
+        button.addEventListener("click", () => {
+            inputOperator(button.value);
+            updateDisplay();
+        });
+    }
+    else if(button.id === "clear") {
+        button.addEventListener("click", () => {
+            clear();
+            updateDisplay();
+        });
+    }
+})
+
+
+function inputOperand(operand) {
+    if (firstOperand === null) {
+        if (displayValue === 0 || displayValue === '0') {
+            displayValue = operand;
+        }
+        else {
+            displayValue += operand;
+        }
+    }
+}
+
+function inputOperator(operator) {
+
+}
+
+function updateDisplay() {
+    if (displayValue.length < 9) {
+        display.textContent = displayValue;
+    }
+    else {
+        display.textContent = displayValue.substring(0, 9);
+    }
+}
+
+function clear() {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    operater = null;
+    result = '0';
+}
